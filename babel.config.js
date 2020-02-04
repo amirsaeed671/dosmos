507c8f7ffe9e63d45b038d16d3c4ca551bee27fd
Amir Ali <amir.saeed.ali92@gmail.com>
2020-02-04 17:40:04 +0500

    testing enviroment setup

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+// babel.config.js
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+      [
+        '@babel/preset-react',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+  };
