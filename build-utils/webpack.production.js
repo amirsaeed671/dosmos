fde7e3e20dc369e9a5f8d30abb0590485779cc5c
Amir Ali <amir.saeed.ali92@gmail.com>
2020-06-15 16:45:35 +0500

    base webpack setup

---
+++ build-utils/webpack.production.js
@@ -0,0 +1,18 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = () => ({
+    output: {
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader , 'css-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ]
+});
