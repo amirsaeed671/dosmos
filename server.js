d9c4340e9c9ef4376cb9bc086e32b5a60d24a2fa
Amir Ali <amir.saeed.ali92@gmail.com>
2020-06-17 15:17:55 +0500

    changes for heroku

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const port = process.env.PORT || 8080
 
 app.use('/', express.static(path.join(__dirname, 'dist')))
 
-app.listen(port, () => console.log("Listening on Port", port)) 
+app.listen(port, () => console.log('Listening on Port', port))
