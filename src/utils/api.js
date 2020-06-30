82cef4e5e224afc24cd9c5239ddcbb3516d41e45
Amir Ali <amir.saeed.ali92@gmail.com>
2020-06-30 17:56:22 +0500

    env changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {getUser, isLoggedIn} from 'auth'
 
-const baseURL = process.env.REACT_APP_BASE_URL
+const baseURL = 'https://dosmos-backend.herokuapp.com/'
 
 const axiosInstance = axios.create({baseURL})
 
